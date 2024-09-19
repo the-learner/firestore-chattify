1a9dcba8cc56a30435aca5874ee233d3a65ed0a0
Shubham Solanki <shubhamsolanki@google.com>
2024-09-19 09:12:56 +0000

    Added google config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
       provideFirebaseApp(() => initializeApp(environment.firebase)),
       provideFirestore(() => getFirestore()),
       provideAuth(() => getAuth()),
       provideFunctions(() => getFunctions()),
       provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"shubhamsolanki-test","appId":"1:60288896955:web:4ce7c8086ffd08040e7e9e","storageBucket":"shubhamsolanki-test.appspot.com","locationId":"us-central","apiKey":"AIzaSyAjooi-xS3C70eBHrn0QYQpAj5_0c-pprI","authDomain":"shubhamsolanki-test.firebaseapp.com","messagingSenderId":"60288896955","measurementId":"G-X3LL9DTFFQ"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+      provideMessaging(() => getMessaging()),
+  provideRouter(routes)
   ],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyAjooi-xS3C70eBHrn0QYQpAj5_0c-pprI",
+    authDomain: "shubhamsolanki-test.firebaseapp.com",
+    databaseURL: 'https://shubhamsolanki-test.firebaseio.com',
+    projectId: "shubhamsolanki-test",
+    storageBucket: "shubhamsolanki-test.appspot.com",
+    messagingSenderId: "60288896955",
+    appId: "1:60288896955:web:4ce7c8086ffd08040e7e9e",
+    measurementId: "G-X3LL9DTFFQ"
   },
 };
